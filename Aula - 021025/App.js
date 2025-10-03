15b783a6e330a00d355616844a24d3c4abe2baef
Lucas Landgraf <lucaslandgraaf@gmail.com>
2025-10-02 23:50:15 -0300

    Aula - 021025

---
+++ Aula - 021025/App.js
@@ -0,0 +1,11 @@
+import MainStack from './src/navigation/MainStack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
+}
