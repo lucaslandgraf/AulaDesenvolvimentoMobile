e8633e6a900bc551e03aa6725b5e9ccd15b4699d
Lucas Landgraf <lucaslandgraaf@gmail.com>
2025-10-02 23:49:45 -0300

    Delete Aula - 021025 directory

--- Aula - 021025/src/screens/SettingsUser.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native"
-
-export default function SettingsUser(){
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignContent: "center"}}>
-            <Text style={{fontSize: 22, fontWeight: "500", textAlign: 'center'}}>Configurações do Usuário</Text>
-        </View>
-    )
-}
